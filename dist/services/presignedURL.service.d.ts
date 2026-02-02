321d6faa61e6024ab8b1eaa70d068d6dbd69861e
Daejun <eownsrla03@naver.com>
2026-02-03 00:02:21 +0900

    Dockerfile 추가

--- dist/services/presignedURL.service.d.ts
+++ dist/services/presignedURL.service.d.ts
@@ -1,7 +1,6 @@
-import { S3Client } from "@aws-sdk/client-s3";
 type Operation = "PUT" | "GET";
 export declare class PresignedURLService {
-    s3Client: S3Client;
+    private s3Client;
     private isNonEmptyString;
     getPresignedURL: (domain: string, // 폴더 이름
     operation: Operation, fileName: string, fileType: string) => Promise<string>;
